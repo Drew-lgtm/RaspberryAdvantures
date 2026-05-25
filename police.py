85aef8d4781015dd3b5627c7552e2608722034e9
Ondrej Martinek <martinek.ondrej.cz@gmail.com>
2026-05-25 13:10:42 +0200

    Correct sleep intervals, comments, and typos in blinker1s.py and police.py

--- police.py
+++ police.py
@@ -5,10 +5,9 @@ red_led = machine.Pin(17, machine.Pin.OUT)
 blue_led = machine.Pin(28, machine.Pin.OUT)
 
 while True:
-    blue_led.value(1)# turn on led
-    red_led.value(0)# turn on led
-    utime.sleep(0.5)# sleep 1 sec
-    red_led.value(1)# turn on led
-    blue_led.value(0)# turn led of
-    utime.sleep(0.5)
-    
+    blue_led.value(1)# turn on blue led
+    red_led.value(0)# turn off red led
+    utime.sleep(0.5)# sleep 0.5 sec
+    red_led.value(1)# turn on red led
+    blue_led.value(0)# turn off blue led
+    utime.sleep(0.5)# sleep 0.5 sec
